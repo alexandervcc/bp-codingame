35335844d04cb55f1299ef90ca8f82d056a2ec24
Alex Charco Cuenca <alex.charco@devsu.com>
2022-08-22 00:02:36 -0500

    Cors

---
+++ src/main/java/acc/spring/config/AppConfig.java
@@ -0,0 +1,20 @@
+package acc.spring.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class AppConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("PUT", "DELETE", "GET", "POST")
+                .allowCredentials(false).maxAge(3600);
+    }
+
+}
